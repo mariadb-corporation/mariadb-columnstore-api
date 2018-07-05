bebc1d83ac642ba0749fbff5aacb0cc7a0085f65
sneJ- <jens.rowekamp@mariadb.com>
2018-07-05 13:43:39 -0700

    MCOL-1524 - avoid infinity loop by nested comatibility tests

--- java/test/compatibility_test.sh
+++ java/test/compatibility_test.sh
@@ -22,7 +22,7 @@ for tag in $( git tag -l columnstore-$MAJOR_VERSION.$MINOR_VERSION.* ); do
         mv mariadb-columnstore-api $tag
         cd $tag
         git checkout tags/$tag
-        cmake -DTEST_RUNNER=ON -DSPARK_CONNECTOR=OFF -DPYTHON=OFF -DJAVA=ON -DRUN_CPPCHECK=OFF -DBUILD_DOCS=OFF .
+        cmake -DTEST_RUNNER=ON -DSPARK_CONNECTOR=OFF -DPYTHON=OFF -DJAVA=ON -DRUN_CPPCHECK=OFF -DBUILD_DOCS=OFF -DJAVA_COMPATIBILITY_TEST=OFF .
         make -j2
         cd $DIR
 
@@ -49,7 +49,7 @@ cd $DIR
 failed=0
 
 for tag in $( git tag -l columnstore-$MAJOR_VERSION.$MINOR_VERSION.* ); do
-    #filter versions prior 1.1.2 as these are unstable
+    #filter versions prior 1.1.3 as these are unstable and use _javamcsapi.so instead of javamcsapi.so
     IFS='.' read -r -a CHECK_VERSION <<< ${tag:12}
     if [ ${CHECK_VERSION[0]} -ge 2 -o ${CHECK_VERSION[1]} -ge 2 -o ${CHECK_VERSION[2]} -ge 3 ]; then
         # verify that all original Java tests still pass with the original mcsapi and original javamcsapi (baseline)
