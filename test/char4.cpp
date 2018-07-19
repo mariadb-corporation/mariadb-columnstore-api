41239ea998e44f43993f1923f705c7a8283d6377
sneJ- <jens.rowekamp@mariadb.com>
2018-07-18 21:02:44 -0700

    MCOL-1281 - Windows tests, MSI package build, and documentation

--- test/char4.cpp
+++ test/char4.cpp
@@ -29,10 +29,22 @@ class TestEnvironment : public ::testing::Environment {
   // Override this to define how to set up the environment.
   virtual void SetUp()
   {
+    std::string cs_ip = "127.0.0.1";
+	std::string cs_user = "root";
+	std::string cs_password = "";
+    if(std::getenv("MCSAPI_CS_TEST_IP")){
+        cs_ip = std::getenv("MCSAPI_CS_TEST_IP");
+    }
+	if(std::getenv("MCSAPI_CS_TEST_USER")){
+        cs_user = std::getenv("MCSAPI_CS_TEST_USER");
+    }
+	if(std::getenv("MCSAPI_CS_TEST_PASSWORD")){
+        cs_password = std::getenv("MCSAPI_CS_TEST_PASSWORD");
+    }
     my_con = mysql_init(NULL);
     if (!my_con)
         FAIL() << "Could not init MariaDB connection";
-    if (!mysql_real_connect(my_con, "127.0.0.1", "root", "", NULL, 3306, NULL, 0))
+    if (!mysql_real_connect(my_con, cs_ip.c_str(), cs_user.c_str(), cs_password.c_str(), NULL, 3306, NULL, 0))
         FAIL() << "Could not connect to MariaDB: " << mysql_error(my_con);
     if (mysql_query(my_con, "CREATE DATABASE IF NOT EXISTS mcsapi"))
         FAIL() << "Error creating database: " << mysql_error(my_con);
