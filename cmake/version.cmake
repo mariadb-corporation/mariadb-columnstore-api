e80bc075923eaa42d373d59e79ef79ad7a0a23d9
david hill <david.hill@mariadb.com>
2018-11-15 09:21:33 -0600

    update version

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -4,7 +4,7 @@ set(MCSAPI_VERSION_MAJOR 1)
 # Incement if source code has changed, zero if major is incremented
 set(MCSAPI_VERSION_MINOR 2)
 # Increment if interfaces have beed added, set to zero if minor is changed
-set(MCSAPI_VERSION_PATCH 1)
+set(MCSAPI_VERSION_PATCH 2)
 set(MCSAPI_VERSION_STRING ${MCSAPI_VERSION_MAJOR}.${MCSAPI_VERSION_MINOR}.${MCSAPI_VERSION_PATCH})
 
 # Version number to echo
@@ -15,7 +15,7 @@ exec_program("git"
 
 set(VERSION_MAJOR 1)
 set(VERSION_MINOR 2)
-set(VERSION_PATCH 1)
+set(VERSION_PATCH 2)
 set(VERSION_SHORT "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 set(VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}-${GIT_VERSION}")
 
