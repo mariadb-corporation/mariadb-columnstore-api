9ba87e7f8bfa6bf044e434e3e201bcf7ada64e86
benthompson15 <ben.thompson.015@gmail.com>
2020-08-10 11:35:42 -0500

    MCOL-4101: fix path of xml path issue.

--- mcsimport/test/alt_columnstore_con/prepare.py
+++ mcsimport/test/alt_columnstore_con/prepare.py
@@ -4,8 +4,10 @@ def prepare_test(test_directory):
 
     cs_xml = "/etc/columnstore/Columnstore.xml"
     if os.environ.get("COLUMNSTORE_XML_DIR") is not None:
-        cs_xml=os.path.join(os.environ.get("COLUMNSTORE_XML_DIR"),'/Columnstore.xml')
-    
+        cs_xml=os.environ.get("COLUMNSTORE_XML_DIR")+"/Columnstore.xml"
+
+    print("cs_xml file %s" %(cs_xml))
+
     if os.path.exists(cs_xml):
        copyfile(cs_xml,os.path.join(test_directory,'/Columnstore.xml'))
     else:
